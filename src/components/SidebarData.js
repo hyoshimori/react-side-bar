763a7651413bdb091848e888ab4c77b864e7b480
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2022-12-13 17:56:12 +0100

    li and linkpath added

---
+++ src/components/SidebarData.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import EmailIcon from '@mui/icons-material/Email';
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import AddCardIcon from "@mui/icons-material/AddCard";
+import BackupIcon from "@mui/icons-material/Backup";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    icon: <HomeIcon />,
+    Link: "./home"
+  },
+  {
+    title: "Mail",
+    icon: <EmailIcon />,
+    Link: "./mail"
+  },
+  {
+    title: "Analitics",
+    icon: <AssessmentIcon />,
+    link: "/analitics",
+  },
+  {
+    title: "Friends",
+    icon: <PersonAddIcon />,
+    link: "/friends",
+  },
+  {
+    title: "Payment",
+    icon: <AddCardIcon />,
+    link: "/payment",
+  },
+  {
+    title: "Upload",
+    icon: <BackupIcon />,
+    link: "/upload",
+  },
+  {
+    title: "Setting",
+    icon: <SettingsIcon />,
+    link: "/Setting",
+  },
+]
